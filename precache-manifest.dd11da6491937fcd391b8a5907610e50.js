ae3b88ddfb934ff74005a72ab10d1269b0429b34
Saeed Shareef <saeedhshareef@gmail.com>
2020-08-05 15:48:54 -0400

    Updates

---
+++ precache-manifest.dd11da6491937fcd391b8a5907610e50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d06ed715e6afddda7ec22246de8898",
+    "url": "/homeworkAug3/index.html"
+  },
+  {
+    "revision": "61b32d2a79daf4b070a6",
+    "url": "/homeworkAug3/static/css/main.cf16eb8d.chunk.css"
+  },
+  {
+    "revision": "b6ce20bb6c20149b8032",
+    "url": "/homeworkAug3/static/js/2.7ec3d40f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/homeworkAug3/static/js/2.7ec3d40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61b32d2a79daf4b070a6",
+    "url": "/homeworkAug3/static/js/main.117d5a9c.chunk.js"
+  },
+  {
+    "revision": "72c5a1dd97b4c0585b4c",
+    "url": "/homeworkAug3/static/js/runtime-main.b681691e.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/homeworkAug3/static/media/pokeball.f10bf078.png"
+  }
+]);
